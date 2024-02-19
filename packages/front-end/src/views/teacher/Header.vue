c98c2a6c28d35bd91513775a78a12e1e8306b5f3
Grosia <105112262+Grosia@users.noreply.github.com>
2024-02-19 13:20:42 +0800

    Add files via upload

---
+++ packages/front-end/src/views/teacher/Header.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Nav from "@components/Nav.vue";
+import { ref } from "vue";
+import type {NavItem} from "@components/Nav";
+
+const index = ref<number>(0);
+const items = <NavItem[]>[
+  {name:"申请处理", path:"/teacher"},
+  {name:"学生名单", path:"/teacher/slist"},
+  {name:"密码修改", path:"/teacher/cipher"},
+];
+
+</script>
+<template>
+  <div class="base bg-blue-300 flex">
+    <Nav
+      v-model="index"
+      :items="items"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.base{
+  height: 50px;
+}
+</style>
