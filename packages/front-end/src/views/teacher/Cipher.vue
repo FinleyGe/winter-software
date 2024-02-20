680c7843ad93bddae014cbf0da083a42f4cc98cf
1360435871@qq.com <linyiyou@MacBook-Pro-2.local>
2024-02-20 16:35:18 +0800

    feat(front-end): teacher header

---
+++ packages/front-end/src/views/teacher/Cipher.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { Form } from "@api/type";
+
+const student_info = ref<Form | null>();
+
+</script>
+<template>
+  TODO: Cipher change of teacher
+  {{ student_info }}
+</template>
+
+<style scoped lang="scss">
+
+</style>
